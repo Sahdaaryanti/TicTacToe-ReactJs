da92094739cf5007dde9384ed0dd6bddb53e249b
sahdaaryanti <nurafni@nurs-MacBook-Air.local>
2023-11-03 14:27:23 +0800

    fix tic-tac-toe display

--- src/App.js
+++ src/App.js
@@ -69,7 +69,6 @@ function Game() {
   );
 }
 
-// eslint-disable-next-line no-unused-vars
 function calculateStatus(winner, squares, nextValue) {
   return winner
     ? `Winner: ${winner}`
@@ -78,12 +77,10 @@ function calculateStatus(winner, squares, nextValue) {
       : `Next player: ${nextValue}`;
 }
 
-// eslint-disable-next-line no-unused-vars
 function calculateNextValue(squares) {
   return squares.filter(Boolean).length % 2 === 0 ? 'X' : 'O';
 }
 
-// eslint-disable-next-line no-unused-vars
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
